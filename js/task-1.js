836ee42a193651ff960931b987aab01863cd61cd
huralomer <huralomer@gmail.com>
2025-02-12 16:26:18 +0300

    task güncellemesi

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid) {
   let totalPrice = quantity * pricePerDroid;
   if (quantity > 1) {
-    return `"You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   } else {
-    return `"You ordered ${quantity} droid worth ${totalPrice} credits!`;
+    return `You ordered ${quantity} droid worth ${totalPrice} credits!`;
   }
 }
 console.log(makeTransaction(5, 3000));
